2c429892661f4735b7b630deed38cf20b1a63552
Denis Krinitsyn <skylinekrin@gmail.com>
2022-12-15 23:01:45 +0500

    Update int to doubleArray

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -1,23 +1,23 @@
 ﻿int N = new Random().Next(3, 10); //Рандом расчет длины массива
-int min, max;
-int[] Array = InitializeRandomArray(N); //Инициализируем массив рандомными числами
-int res = CalcDiffMinMax(Array, out min, out max); //Метод расчета min и max и их разницы
+double min, max;
+double[] Array = InitializeRandomArray(N); //Инициализируем массив рандомными числами
+double res = CalcDiffMinMax(Array, out min, out max); //Метод расчета min и max и их разницы
 Console.WriteLine("Разница между max({0}) и min({1}): {2}",max, min, res);//Выводим разницу между max и min
 PrintArray(Array);//Печатаем массив для проверки
 
 //Метод печати массива
-void PrintArray(int[] arr)
+void PrintArray(double[] arr)
 {
     Console.Write("[");
     for (int i = 0; i < arr.Length; i++)
     {
         if (i == 0) Console.Write(arr[i]);
-        else Console.Write(", " + arr[i]);
+        else Console.Write("; " + arr[i]);
     }
     Console.Write("]");
 }
 //Метод расчета разницы между max и min
-int CalcDiffMinMax(int[] Array, out int min, out int max)
+double CalcDiffMinMax(double[] Array, out double min, out double max)
 {
     min = Array[0];
     max = Array[0];
@@ -27,17 +27,17 @@ int CalcDiffMinMax(int[] Array, out int min, out int max)
         if (Array[i] < min) min = Array[i];
         if (Array[i] >= max) max = Array[i];
     }
-    int res = max - min;
+    double res = max - min;
     return res;
 }
 //Инициализацияя рандомного массива
-int[] InitializeRandomArray(int N)
+double[] InitializeRandomArray(int N)
 {
     Random rnd = new Random();
-    int[] arr = new int[N];
+    double[] arr = new double[N];
     for (int i = 0; i < N; i++)
     {
-        arr[i] = rnd.Next(-99, 100);
+        arr[i] = Math.Round(rnd.NextDouble() * 100, 2);
     }
     return arr;
 }
